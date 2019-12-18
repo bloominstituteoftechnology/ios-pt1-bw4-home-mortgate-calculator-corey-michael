8e288893d4c98fea0de365f055d3718ac937900e
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-12-17 23:19:35 -0500

    Added project files

---
+++ Mortgage Calculator/Mortgage Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mortgage Calculator
+//
+//  Created by Michael Stoffer on 12/17/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
